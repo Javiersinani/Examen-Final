221edcc97111a5527d0922b46f3b0813c632f74a
Javier Siñani Lijeron <a2019113929@estudiantes.upsa.edu.bo>
2021-06-19 10:04:14 -0400

    Cracion del programa

---
+++ lib/main.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MiApp());
+
+class MiApp extends StatelessWidget {
+  const MiApp({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Mi App",
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Inicio(),
+    );
+  }
+}
+
+class Inicio extends StatefulWidget {
+  Inicio({Key key}) : super(key: key);
+
+  @override
+  _InicioState createState() => _InicioState();
+}
+
+class _InicioState extends State<Inicio> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("Javier App"),
+        ),
+        body: Column(
+          children: <Widget>[
+            Center(
+              child: Text("Javier Siñani Lijeron"),
+            ),
+          ],
+        ));
+  }
+}
